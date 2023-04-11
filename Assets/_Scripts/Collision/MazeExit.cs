47a8c873fad33ffbc2286172177f6ad8801ff464
Muhammet İNCE <83353864+MuhammetInceee@users.noreply.github.com>
2023-04-11 19:21:38 +0300

    Graphy Package Added

--- Assets/_Scripts/Collision/MazeExit.cs
+++ Assets/_Scripts/Collision/MazeExit.cs
@@ -28,12 +28,15 @@ namespace FallingBalls.Physics
         private void InitVariables()
         {
             _tempBallCount = _mazeController.ActiveBalls;
+            print(_tempBallCount);
         }
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.TryGetComponent(out BallManager ballManager))
             {
+                print(_tempBallCount);
+                
                 _tempBallCount--;
                 
                 if (_tempBallCount <= 1)
